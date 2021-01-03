3403fce858d9202544c052aa7128801f3ee17dc9
Mehemmed <63582023+Maga03@users.noreply.github.com>
2021-01-03 18:30:51 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+export const mainstyle={p:{color:''},ul:{listStyleType:'none'},a:{padding:'0 20px',cursor:'pointer'},container:{width:'90%',margin:'0 auto',padding:'10px 10px'}}
+export const test="test"
